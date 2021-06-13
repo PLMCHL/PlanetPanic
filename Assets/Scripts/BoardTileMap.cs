c19c99d97a58ec671ad5d15f7de6b654b21e1dae
PLMCHL <pm.deblois@gmail.com>
2021-06-13 10:48:38 -0700

    Changing board to be based on graph

---
+++ Assets/Scripts/BoardTileMap.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+class BoardTileMap
+{
+    private List<int> tileDirectionList = new List<int> { 0, 5, 5, 0, 1, 0, 5, 5, 0, 1, 0, 1, 1, 2, 3, 3, 2, 1, 2, 2, 3, 3, 4, 5, 4, 3, 4, 4 };
+
+    private List<Vector3Int> DIRECTIONS = new List<Vector3Int>
+    {
+        new Vector3Int(0, -1, 1),
+        new Vector3Int(1, -1, 0),
+        new Vector3Int(1, 0, -1),
+        new Vector3Int(0, 1, -1),
+        new Vector3Int(-1, 1, 0),
+        new Vector3Int(-1, 0, 1)
+    };
+
+    public TileNode launch { get; private set; }
+    public TileNode start { get; private set; }
+
+    public BoardTileMap(Vector3Int launch)
+    {
+        this.launch = new TileNode(launch);
+        InitializeTiles();
+    }
+
+    void InitializeTiles()
+    {
+        var currTile = launch;
+
+        foreach (var dir in tileDirectionList)
+        {
+            var nextTile = new TileNode(currTile.Location + DIRECTIONS[dir]);
+
+            if (start == null)
+            {
+                start = nextTile;
+            }
+
+            currTile.nextTiles = new List<TileNode> { nextTile };
+
+            currTile = nextTile;
+        }
+    }
+}
---
+++ Assets/Scripts/GridBehaviour.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridBehaviour : MonoBehaviour
+{
+    public TileBase grass;
+    public TileBase dirt;
+    public TileBase lava;
+    public TileBase magic;
+    public TileBase rock;
+    public TileBase sand;
+
+    public Tilemap tileMap;
+
+    private BoardTileMap boardTileMap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boardTileMap = new BoardTileMap(new Vector3Int(0, 0, 0));
+        SetTilesPath();
+    }
+
+    void SetTilesPath()
+    {
+        setTile(boardTileMap.launch.Location, sand);
+
+        recursiveFill(new List<TileNode>{ boardTileMap.start });
+
+    }
+
+    void recursiveFill(List<TileNode> tiles)
+    {
+        foreach(TileNode tile in tiles)
+        {
+            setTile(tile.Location, grass);
+
+            recursiveFill(tile.nextTiles);
+        }
+    }
+
+    private Vector3Int CubeToUnityCell(Vector3Int cube)
+    {
+        var x = cube.x;
+        var z = cube.z;
+        var col = x + (z - (z & 1)) / 2;
+        var row = z;
+
+        return new Vector3Int(col, row, 0);
+    }
+
+    private void setTile(Vector3Int loc, TileBase tileType)
+    {
+        tileMap.SetTile(CubeToUnityCell(loc), tileType);
+    }
+}
