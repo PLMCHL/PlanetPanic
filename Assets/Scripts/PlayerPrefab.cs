9f1a45b7b4472dfcdfa5fcf842f6e9f153691d0e
PLMCHL <pm.deblois@gmail.com>
2022-02-04 11:40:25 -0800

    Randomize characters

--- Assets/Scripts/PlayerPrefab.cs
+++ Assets/Scripts/PlayerPrefab.cs
@@ -3,11 +3,19 @@ using UnityEngine;
 
 public class PlayerPrefab : MonoBehaviour
 {
-    public static int LastUsedIndex = 0;
+    public static int LastUsedIndex;
     public List<Sprite> sprites;
+
+    private static bool initialized;
 
     void Start()
     {
+        if (!initialized)
+        {
+            LastUsedIndex = Random.Range(0, sprites.Count);
+            initialized = true;
+        }
+
         var spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = sprites[LastUsedIndex++];
     }
